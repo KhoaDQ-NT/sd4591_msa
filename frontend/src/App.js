3ce1099dd51bc52cca7991cf55e1a1a9426763a0
Khoa Dang Quoc <khoadangq@nashtechglobal.com>
2023-08-06 08:33:06 +0700

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import axios from "axios";
-import "./App.scss";
-import AddTodo from "./components/AddTodo";
-import TodoList from "./components/TodoList";
+import React from 'react';
+import axios from 'axios';
+import './App.scss';
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -15,34 +15,34 @@ export default class App extends React.Component {
 
   componentDidMount() {
     axios
-      .get("/api")
+      .get('/api')
       .then((response) => {
         this.setState({
           todos: response.data.data,
         });
       })
-      .catch((e) => console.log("Error : ", e));
+      .catch((e) => console.log('Error : ', e));
   }
 
   handleAddTodo = (value) => {
     axios
-      .post("/api/todos", { text: value })
+      .post('/api/todos', { text: value })
       .then(() => {
         this.setState({
           todos: [...this.state.todos, { text: value }],
         });
       })
-      .catch((e) => console.log("Error : ", e));
+      .catch((e) => console.log('Error : ', e));
   };
 
   render() {
     return (
-      <div className="App container">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-xs-12 col-sm-8 col-md-8 offset-md-2">
-              <h1>Todos</h1>
-              <div className="todo-app">
+      <div className='App container'>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-xs-12 col-sm-8 col-md-8 offset-md-2'>
+              <h1>Todos K</h1>
+              <div className='todo-app'>
                 <AddTodo handleAddTodo={this.handleAddTodo} />
                 <TodoList todos={this.state.todos} />
               </div>
